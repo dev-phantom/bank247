4da06b08ac548c3571001196a4360e157ef9be77
dev-phantom <godsfavour1975@gmail.com>
2023-07-18 00:49:33 +0100

    fix clients correction

--- src/components/home/learnMore.jsx
+++ src/components/home/learnMore.jsx
@@ -1,22 +1,18 @@
-
 export default function LearnMore() {
   return (
     <div className="w-full flex justify-center items-center pb-5">
       <div className="w-[90%] bg-blue-700 px-10 py-12 rounded-2xl">
-        <div className="w-[60%] text-white">
+        <div className="w-[70%] text-white">
           <h1 className="py-3 text-5xl font-ubuntu font-bold leading-[4rem]">
-            Trust the small business experts that drive growth.
+            Trust Bank247 to help you meet your funding expectation.
           </h1>
-          <p className="py-5 text-xl font-ubuntu font-normal leading-[2rem]">
-            Our unique mix of deep industry experience, smart technology,
-            personalized service and backing by a top-performing NASDAQ company
-            is unmatched anywhere. And the experts agree, as we’re recognized by
-            Entrepreneur magazine as a Top Franchise Supplier back-to-back
-            years. We are honored.
+          <p className="py-5 pr-10 text-xl font-ubuntu font-normal leading-[2rem]">
+            Our broad network of financial institutions and expert advisors,
+            combined with our proven financial technology put you in charge every time, every where. 
           </p>
-          <div >
+          <div>
             <button className="py-2 font-bold uppercase font-openSan rounded-full px-8 bg-[#FF5700] text-white">
-              see about bank247
+              learn more
             </button>
           </div>
         </div>
